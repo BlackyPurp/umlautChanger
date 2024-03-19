22dc6d9da7e8d7a5399ba470d7f8a49b8a3b7d25
BlackyPurp <jasonvolz@hotmail.de>
2024-03-19 11:02:04 +0100

    created repo

---
+++ umlaut.py
@@ -0,0 +1,18 @@
+import sys
+
+if(len(sys.argv) <= 1):
+    print('No file given, please use as such: "umlaut.py {file}"')
+    exit(1)
+else:
+    with open(sys.argv[1],'r') as file:
+        filedata = file.read()
+        filedata = filedata.replace('ae','ä')
+        filedata = filedata.replace('oe','ö')
+        filedata = filedata.replace('ue','ü')
+
+        filedata = filedata.replace('Ae','Ä')
+        filedata = filedata.replace('Oe','Ö')
+        filedata = filedata.replace('Ue','Ü')
+    with open(sys.argv[1],'w') as file:
+        file.write(filedata)
+        print('Succesfully Changed')
